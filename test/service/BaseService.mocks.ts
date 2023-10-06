e05791d5d87ec07c281251a108d5ced4626648d0
zhamdi <zied@weally.org>
2023-10-06 15:49:12 +0200

    BaseService.mocks.ts added to extract mocks, but tests are broken
      ● Test suite failed to run
    
        test/service/BaseService.test.ts:154:28 - error TS2339: Property 'toHaveSameFields' does not exist on type 'Matchers<void, IOffer<ObjectId> | null> & SnapshotMatchers<void, IOffer<ObjectId> | null> & Inverse<JestMatchers<void, IOffer<...> | null>> & PromiseMatchers<...>'.
    
        154     expect(retrievedOffer).toHaveSameFields(createdOffer);

---
+++ test/service/BaseService.mocks.ts
@@ -0,0 +1,120 @@
+import { Types } from "mongoose";
+
+import { IDaoFactory } from "../../src/db/dao";
+import { IOffer, ISubscription } from "../../src/db/model";
+import { TestContainerSingleton } from "../testContainer";
+
+export type ObjectId = Types.ObjectId;
+
+function newObjectId(): ObjectId {
+  return new Types.ObjectId();
+}
+
+type InitMocksResult = {
+  daoFactoryMock: IDaoFactory<ObjectId>;
+  dbUri: string;
+  mongooseDaoFactory: IDaoFactory<ObjectId>;
+  offerChild1: IOffer<ObjectId>;
+  offerChild2: IOffer<ObjectId>;
+  offerRoot1: IOffer<ObjectId>;
+  offerRoot2: IOffer<ObjectId>;
+  sampleUserId: ObjectId;
+  subscriptionPaid1: ISubscription<ObjectId>;
+  subscriptionPending1: ISubscription<ObjectId>;
+  subscriptionRefused1: ISubscription<ObjectId>;
+};
+
+export async function initMocks(): Promise<InitMocksResult> {
+  const testContainer = await TestContainerSingleton.getInstance();
+  // Sample data for testing
+  const sampleUserId: ObjectId = testContainer.resolve("sampleUserId");
+
+  const subscriptionPaid1: ISubscription<ObjectId> = {
+    expires: new Date(),
+    offerId: newObjectId(),
+    starts: new Date(),
+    status: "paid",
+  } as ISubscription<ObjectId>;
+
+  const subscriptionPending1: ISubscription<ObjectId> = {
+    expires: new Date(),
+    offerId: newObjectId(),
+    starts: new Date(),
+    status: "pending",
+  } as ISubscription<ObjectId>;
+
+  const subscriptionRefused1: ISubscription<ObjectId> = {
+    expires: new Date(),
+    offerId: newObjectId(),
+    starts: new Date(),
+    status: "refused",
+  } as ISubscription<ObjectId>;
+
+  const offerRoot1: IOffer<ObjectId> = {
+    _id: newObjectId(),
+    cycle: "once",
+    hasSubOffers: false, // This offer has no sub-offers
+    kind: "tokens",
+    name: "100 tokens for 100$",
+    overridingKey: "100tokens",
+    parentOfferId: null as any, // To be updated below
+    price: 100,
+    tokenCount: 100,
+  } as IOffer<ObjectId>;
+
+  const offerRoot2: IOffer<ObjectId> = {
+    _id: newObjectId(),
+    cycle: "monthly",
+    hasSubOffers: true, // This offer has sub-offers
+    kind: "subscription",
+    name: "Starter",
+    overridingKey: "100tokens",
+    parentOfferId: null as any, // To be updated below
+    price: 50,
+    tokenCount: 0,
+  } as IOffer<ObjectId>;
+
+  const offerChild1: IOffer<ObjectId> = {
+    _id: newObjectId(),
+    cycle: "once",
+    hasSubOffers: false, // This offer has no sub-offers
+    kind: "tokens",
+    name: "20% off on 50 tokens",
+    overridingKey: "50tokens",
+    parentOfferId: offerRoot2._id, // is a sub-offer of Starter offer
+    price: 40,
+    tokenCount: 50,
+  } as IOffer<ObjectId>;
+
+  const offerChild2: IOffer<ObjectId> = {
+    _id: newObjectId(),
+    cycle: "once",
+    hasSubOffers: false, // This offer has no sub-offers
+    kind: "tokens",
+    name: "30% off on 100 tokens",
+    overridingKey: "100tokens",
+    parentOfferId: offerRoot2._id, // is a sub-offer of Starter offer overrides the 100tokens offer
+    price: 70,
+    tokenCount: 100,
+  } as IOffer<ObjectId>;
+
+  const daoFactoryMock: IDaoFactory<ObjectId> =
+    testContainer.resolve("daoFactoryMock");
+  testContainer.resolve("mongoServer");
+  const mongooseDaoFactory: IDaoFactory<ObjectId> =
+    testContainer.resolve("mongooseDaoFactory");
+
+  return {
+    daoFactoryMock,
+    dbUri: testContainer.resolve("dbUri"),
+    mongooseDaoFactory,
+    offerChild1,
+    offerChild2,
+    offerRoot1,
+    offerRoot2,
+    sampleUserId,
+    subscriptionPaid1,
+    subscriptionPending1,
+    subscriptionRefused1,
+  };
+}
