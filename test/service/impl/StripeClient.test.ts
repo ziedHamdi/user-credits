d93776769c84c45904933b655adaff3726253087
zhamdi <zied@weally.org>
2023-10-15 23:25:18 +0200

    Tests pass for StripeClient.ts

--- test/service/impl/StripeClient.test.ts
+++ test/service/impl/StripeClient.test.ts
@@ -1,14 +1,20 @@
 import { beforeEach, describe, it, jest } from "@jest/globals";
 import { expect } from "expect";
 
-import { IOrder } from "../../../src/db/model";
+import { PaymentError } from "../../../src/errors";
 import { StripeClient } from "../../../src/impl/service/StripeClient";
 import { IConfigReader } from "../../../src/service/config/IConfigReader";
 import {
+  clearStripeMocks,
   constructEventMock,
   paymentIntentsCreateMock,
   paymentIntentsRetrieveMock,
+  prepareAfterPaymentExecutedMock,
+  prepareCreatePaymentIntentMock,
 } from "./mocks/StripeMocks";
+import { IOrder } from "../../../src/db/model";
+
+const intentId = "payment_intent_id";
 
 jest.mock("stripe", () => {
   return jest.fn().mockImplementation(() => {
@@ -35,29 +41,24 @@ describe("StripeClient", () => {
     } as unknown as IConfigReader;
 
     stripeClient = new StripeClient(configReaderMock);
+    clearStripeMocks();
   });
 
   it("should create a payment intent", async () => {
+    const amount: number = 100;
     // Arrange
-    const order: IOrder<string> = {
-      // Initialize order object with necessary properties
-      total: 100,
-    } as unknown as IOrder<string>;
-
-    const expectedPaymentIntent = {
-      client_secret: "client_secret_key",
-      id: "payment_intent_id",
-      status: "requires_payment_method",
-    };
-
-    paymentIntentsCreateMock.mockResolvedValue(expectedPaymentIntent as never);
+    const order = prepareCreatePaymentIntentMock(
+      "requires_payment_method",
+      intentId,
+      amount,
+    );
 
     // Act
     const result = await stripeClient.createPaymentIntent(order);
 
     // Assert
     expect(paymentIntentsCreateMock).toHaveBeenCalledWith({
-      amount: 10000, // 100 dollars in cents
+      amount: amount * 100, // 100 dollars in cents
       currency: "usd", // Replace with your desired currency
       description: expect.stringContaining("Payment for Order"),
     });
@@ -69,87 +70,70 @@ describe("StripeClient", () => {
     );
   });
 
-  it("should handle afterPaymentExecuted when status is 'succeeded'", async () => {
+  it("should handle exception when creating a payment intent", async () => {
     // Arrange
-    const order: IOrder<string> = {
-      paymentIntentId: "payment_intent_id",
-    } as IOrder<string>;
-
-    const expectedPaymentIntent = {
-      id: "payment_intent_id",
-      next_action: null,
-      status: "succeeded",
-    };
-
-    paymentIntentsRetrieveMock.mockResolvedValue(
-      expectedPaymentIntent as never,
+    const order = prepareCreatePaymentIntentMock(
+      "requires_payment_method",
+      "error_case",
     );
+    const error = new Error("Test error");
+    paymentIntentsCreateMock.mockRejectedValue(error);
+
+    // Act and Assert
+    await expect(stripeClient.createPaymentIntent(order)).rejects.toThrow(
+      "Error creating payment intent",
+    );
+  });
+
+  it("should handle afterPaymentExecuted when status is 'succeeded'", async () => {
+    const order = prepareAfterPaymentExecutedMock("succeeded", intentId);
 
     // Act
     const result = await stripeClient.afterPaymentExecuted(order);
 
     // Assert
-    expect(paymentIntentsRetrieveMock).toHaveBeenCalledWith(
-      "payment_intent_id",
-    );
+    expect(paymentIntentsRetrieveMock).toHaveBeenCalledWith(intentId);
     expect(result?.status).toBe("paid");
     expect(result?.history?.length).toBe(1);
     expect(result?.history?.[0]?.status).toBe("paid");
   });
 
   it("should handle afterPaymentExecuted when status is 'requires_payment_method'", async () => {
-    // Arrange
-    const order: IOrder<string> = {
-      paymentIntentId: "payment_intent_id",
-    } as IOrder<string>;
-
-    const expectedPaymentIntent = {
-      id: "payment_intent_id",
-      next_action: null,
-      status: "requires_payment_method",
-    };
-
-    paymentIntentsRetrieveMock.mockResolvedValue(
-      expectedPaymentIntent as never,
-    );
+    const order = prepareAfterPaymentExecutedMock("requires_payment_method", intentId);
 
     // Act
     const result = await stripeClient.afterPaymentExecuted(order);
 
     // Assert
-    expect(paymentIntentsRetrieveMock).toHaveBeenCalledWith(
-      "payment_intent_id",
-    );
+    expect(paymentIntentsRetrieveMock).toHaveBeenCalledWith(intentId);
     expect(result?.status).toBe("refused");
     expect(result?.history?.length).toBe(1);
     expect(result?.history?.[0]?.status).toBe("refused");
   });
 
   it("should handle afterPaymentExecuted when status is 'requires_action'", async () => {
-    // Arrange
-    const order: IOrder<string> = {
-      paymentIntentId: "payment_intent_id",
-    } as IOrder<string>;
-
-    const expectedPaymentIntent = {
-      id: "payment_intent_id",
-      next_action: "action_required",
-      status: "requires_action",
-    };
-
-    paymentIntentsRetrieveMock.mockResolvedValue(
-      expectedPaymentIntent as never,
-    );
+    const order = prepareAfterPaymentExecutedMock("requires_action", intentId);
 
     // Act
     const result = await stripeClient.afterPaymentExecuted(order);
 
     // Assert
-    expect(paymentIntentsRetrieveMock).toHaveBeenCalledWith(
-      "payment_intent_id",
-    );
+    expect(paymentIntentsRetrieveMock).toHaveBeenCalledWith(intentId);
     expect(result?.status).toBe("error");
     expect(result?.history?.length).toBe(1);
     expect(result?.history?.[0]?.status).toBe("error");
   });
+  it("should handle exception when retrieving a payment intent", async () => {
+    // Arrange
+    const order: IOrder<string> = {
+      paymentIntentId: intentId,
+    } as IOrder<string>;
+
+    const error = new Error("Test error");
+    paymentIntentsRetrieveMock.mockRejectedValue(error);
+
+    // Act and Assert
+    await expect(stripeClient.afterPaymentExecuted(order)).rejects.toThrow("Error executing payment");
+  });
+
 });
--- test/service/impl/mocks/StripeMocks.ts
+++ test/service/impl/mocks/StripeMocks.ts
@@ -1,15 +1,18 @@
 import { jest } from "@jest/globals";
 import Mock = jest.Mock;
+import { IOrder } from "../../../../src/db/model";
 
-type PaymentIntentsCreate = (params: any) => Promise<any>;
-type PaymentIntentsRetrieve = (paymentIntentId: string) => Promise<any>;
-type ConstructEvent = (rawBody: string, sig: string, secret: string) => any;
+type PaymentIntentsCreate = (params: never) => Promise<never>;
+type PaymentIntentsRetrieve = (paymentIntentId: string) => Promise<never>;
+type ConstructEvent = (rawBody: string, sig: string, secret: string) => never;
 
 // Create jest.fn mocks with the specified signatures
-export const paymentIntentsCreateMock: Mock<PaymentIntentsCreate> = jest.fn<PaymentIntentsCreate>();
-export const paymentIntentsRetrieveMock: Mock<PaymentIntentsRetrieve> = jest.fn<PaymentIntentsRetrieve>();
-export const constructEventMock: Mock<ConstructEvent> = jest.fn<ConstructEvent>();
-
+export const paymentIntentsCreateMock: Mock<PaymentIntentsCreate> =
+  jest.fn<PaymentIntentsCreate>();
+export const paymentIntentsRetrieveMock: Mock<PaymentIntentsRetrieve> =
+  jest.fn<PaymentIntentsRetrieve>();
+export const constructEventMock: Mock<ConstructEvent> =
+  jest.fn<ConstructEvent>();
 
 export function stripeMockInit() {
   return {
@@ -30,4 +33,43 @@ export function clearStripeMocks() {
   paymentIntentsCreateMock.mockClear();
   paymentIntentsRetrieveMock.mockClear();
   constructEventMock.mockClear();
+}
+
+// StripeMocks.ts
+
+export function prepareCreatePaymentIntentMock(
+  status: string,
+  intentId: string = "payment_intent_id",
+  total: number = 100,
+): IOrder<string> {
+  const expectedPaymentIntent = {
+    client_secret: "client_secret_key",
+    id: intentId,
+    status,
+  };
+
+  paymentIntentsCreateMock.mockResolvedValue(expectedPaymentIntent as never);
+
+  return {
+    total,
+  } as IOrder<string>;
+}
+
+export function prepareAfterPaymentExecutedMock(
+  status: string,
+  intentId: string = "payment_intent_id",
+): IOrder<string> {
+  const expectedPaymentIntent = {
+    client_secret: "client_secret_key",
+    id: intentId || "payment_intent_id",
+    status,
+  };
+
+  paymentIntentsRetrieveMock.mockResolvedValue(expectedPaymentIntent as never);
+
+  const order: IOrder<string> = {
+    paymentIntentId: intentId,
+  } as IOrder<string>;
+
+  return order;
 }
