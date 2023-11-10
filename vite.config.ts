8527f06a48d978df05225781e18cf6d2e4c91a32
zhamdi <zied@weally.org>
2023-11-10 17:01:25 +0100

    Added ESLint config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,24 @@
-import {defineConfig} from 'vite'
-import {resolve} from 'path'
+import { resolve } from "path";
+import { defineConfig } from "vite";
 import dtsPlugin from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        dtsPlugin({
-            insertTypesEntry: true,
-        }),
-    ],
-    build: {
-        target:'es2020',
-        lib: {
-            entry: resolve(__dirname, 'src/index.ts'),
-            formats: ['es']
-        }
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
     },
-    resolve: {
-        alias: {
-            src: resolve('src/')
-        }
+    target: "es2020",
+  },
+  plugins: [
+    dtsPlugin({
+      insertTypesEntry: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      src: resolve("src/"),
     },
-})
+  },
+});
