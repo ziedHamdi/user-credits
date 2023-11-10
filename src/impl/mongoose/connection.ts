42aac328534284bb61b56577571338f589269a70
zhamdi <zied@weally.org>
2023-11-10 13:11:37 +0100

    Adjusted awilix related exports

--- src/impl/mongoose/connection.ts
+++ src/impl/mongoose/connection.ts
@@ -1,9 +1,17 @@
-import mongoose, { ConnectOptions, disconnect, Mongoose } from "mongoose";
+import mongoose, { Connection, ConnectOptions, disconnect, Mongoose } from "mongoose";
 
 let connected: Mongoose | unknown;
 let dbUri: string | null;
 
-export async function connectToDb(uri: string, dbName: string) {
+/**
+ * Calls the mongoose.connect method (that creates a unique connection to the db)
+ * @param uri
+ * @param dbName
+ */
+export async function connectToDb(
+  uri: string,
+  dbName: string,
+): Promise<Connection> {
   if (connected) throw new Error("Already connected to mongoose");
 
   if (!uri || !dbName) {
@@ -14,11 +22,10 @@ export async function connectToDb(uri: string, dbName: string) {
     dbName,
   } as ConnectOptions)) as Mongoose;
   dbUri = uri;
+  return mongoose.connection;
 }
 
 export async function disconnectFromDb() {
-  // cover both cases of mongoose.connect and mongoose.createConnection
-  if (connected != null) await (connected as Mongoose).disconnect();
   await disconnect();
   connected = null;
   dbUri = null;
